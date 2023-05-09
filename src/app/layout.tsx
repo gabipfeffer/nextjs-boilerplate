f12a3ba58f5bd928dc7c51ba54d415811abc2b68
gabipfeffer <gabrielpfeffer@gmail.com>
2023-05-09 14:51:14 -0300

    style: conditional styling for navigation based on pathname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "src/components/Header";
-import SplitScreen from "src/components/SplitScreen";
-import Navigation from "src/components/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,28 +14,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const navigation = [{ link: "/", text: "Home" }];
+  const navigation = [
+    { link: "/", text: "Home" },
+    { link: "/splitscreen", text: "Split Screen" },
+  ];
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header logo={{ src: "/vercel.svg", alt: "vercel logo" }} />
-        <SplitScreen leftWeight={0} rightWeight={1}>
-          <div
-            className={
-              "min-h-full px-5 md:px-10 py-5 md:min-w-[8rem] bg-[rgb(var(--background-start-rgb))]"
-            }
-          >
-            <Navigation navigation={navigation} stacked={true} />
-          </div>
-          <main
-            className={
-              "flex-1 min-h-full px-5 md:px-10 py-5 bg-[rgb(var(--background-main-content))] text-[rgb(var(--foreground-rgb))]"
-            }
-          >
-            {children}
-          </main>
-        </SplitScreen>
+        <Header
+          logo={{ src: "/vercel.svg", alt: "vercel logo" }}
+          navigation={navigation}
+        />
+        <main>{children}</main>
       </body>
     </html>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type NavigationProps = {
   navigation: NavigationLink[];
@@ -9,17 +11,28 @@ type NavigationProps = {
 export type NavigationLink = { link: string; text: string };
 
 const Navigation = ({ navigation, stacked, onClick }: NavigationProps) => {
+  const pathname = usePathname();
+
   return navigation?.length ? (
     <ul className={`${!stacked && "justify-end hidden md:flex md:gap-3"}`}>
       {navigation.map((item) => (
         <li
           key={item.link}
-          className={
-            "text-[rgb(var(--foreground-rgb))] tracking-wider lowercase hover:underline hover:text-gray-900"
-          }
+          className={`tracking-wider lowercase hover:underline hover:text-gray-900 whitespace-nowrap ${
+            stacked && "pb-2"
+          }`}
           onClick={onClick}
         >
-          <Link href={item.link}>{item.text}</Link>
+          <Link
+            className={`${
+              pathname === item.link
+                ? "bg-[rgb(var(--foreground-rgb))] text-[rgb(var(--background-start-rgb))] rounded-2xl text-white py-1 px-2"
+                : "text-[rgb(var(--foreground-rgb))] px-2"
+            }`}
+            href={item.link}
+          >
+            {item.text}
+          </Link>
         </li>
       ))}
     </ul>
